8d72243e27f9735be857001c43593c6a573c79f5
radhitfr <email@kampus.ac.id>
2026-03-07 05:44:58 +0700

    mengerjakan week 4

---
+++ Pertemuan 4/scriptpro.js
@@ -0,0 +1,22 @@
+// PROYEK MINI KALKULATOR BIAYA PRODUKSI
+let biayaBahanBaku = ("Rp" + 7500000);
+let biayaTenagaKerja = ("Rp" + 500000);
+let biayaOverhead = ("Rp" + 350000);
+let jumlahProduksi = (50 + " Unit");
+
+//menampilkan data-data
+console.log("Biaya Bahan Baku : " + biayaBahanBaku)
+console.log("Biaya Tenaga Kerja : " + biayaTenagaKerja)
+console.log("Biaya Overhead : " + biayaOverhead)
+console.log("Jumlah Produksi saat ini  : " + jumlahProduksi)
+
+//perhitungan totalperunit
+let totalperunit = (biayaBahanBaku+biayaTenagaKerja+biayaOverhead)/ jumlahProduksi
+totalperunit = totalperunit.toFixed(2)
+
+// if/else pengecekan jumlah produksi
+if (jumlahProduksi < 100) {
+    console.log("Biaya Tinggi (Ekonomi Skala Kecil")
+} else {
+    console.log("Biaya Efisien")
+}
